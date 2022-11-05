a5721e13d0e80cd07c29b6a1bb249d26faa42fd5
Henry-Cha <sungwon4728@naver.com>
2022-11-05 16:19:12 +0900

    Feat: 프로젝트 생성
    스프링부트 2.7.5 자바 11

---
+++ src/main/java/org/whitebox/howlook/HowlookApplication.java
@@ -0,0 +1,13 @@
+package org.whitebox.howlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowlookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowlookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/whitebox/howlook/HowlookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whitebox.howlook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowlookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
