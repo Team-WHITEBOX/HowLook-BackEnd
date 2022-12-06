159d77dfa972d6464ccd176c5640e5c5e1031e61
JeonJuSeong <mo66jyp@gmail.com>
2022-12-06 21:35:35 +0900

    eval data entity 내 값 직접 지정해주는거 삭제

--- src/main/java/org/whitebox/howlook/domain/evaluation/dto/EvalDataDTO.java
+++ src/main/java/org/whitebox/howlook/domain/evaluation/dto/EvalDataDTO.java
@@ -13,21 +13,21 @@ import org.whitebox.howlook.domain.feed.entity.Feed;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EvalDataDTO {
-    private float averageScore = 0; // 평균점수
+    private float averageScore; // 평균점수
 
-    private float maleScore = 0; // 남성 평균점수
+    private float maleScore; // 남성 평균점수
 
-    private float femaleScore = 0; // 여성 평균점수
+    private float femaleScore; // 여성 평균점수
 
-    private float maxScore = 0; // 최고 점수
+    private float maxScore; // 최고 점수
 
-    private float minScore = 0; // 최저 점수
+    private float minScore; // 최저 점수
     
-    private Long replyCount = 0L; // 평가 개수
+    private Long replyCount; // 평가 개수
 
-    private Long maleCount = 0L; // 남자 평가 개수
+    private Long maleCount; // 남자 평가 개수
 
-    private Long femaleCount = 0L; // 여자 평가 개수
+    private Long femaleCount; // 여자 평가 개수
 
     private float[] maleScores = new float[5];
     private float[] femaleScores = new float[5];
