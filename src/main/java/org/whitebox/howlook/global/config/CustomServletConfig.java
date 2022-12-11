c9bfc51a5780fda7238ef17877dce28863ae928c
Henry-Cha <sungwon4728@naver.com>
2022-12-12 00:02:33 +0900

    Feat : 소셜로그인 웹뷰

--- src/main/java/org/whitebox/howlook/global/config/CustomServletConfig.java
+++ src/main/java/org/whitebox/howlook/global/config/CustomServletConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CustomServletConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
-        //registry.addResourceHandler("/files/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/account/**").addResourceLocations("classpath:/templates/account/");
     }
 }
