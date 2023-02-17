692a1a3eaf24a9afeceeff0b5380b3485ee66d1a
Henry-Cha <sungwon4728@naver.com>
2023-02-17 18:16:07 +0900

    Chore: loginDTO api프로퍼티 수정

--- src/main/java/org/whitebox/howlook/domain/member/dto/loginDTO.java
+++ src/main/java/org/whitebox/howlook/domain/member/dto/loginDTO.java
@@ -1,11 +1,15 @@
 package org.whitebox.howlook.domain.member.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class loginDTO {
+    @ApiModelProperty(value = "아이디", example = "testuser1", required = true)
     String memberId;
+
+    @ApiModelProperty(value = "비밀번호", example = "a1234567", required = true)
     String memberPassword;
 }
