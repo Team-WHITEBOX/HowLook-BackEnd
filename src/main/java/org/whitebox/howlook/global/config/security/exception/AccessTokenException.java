b8c93ea54eed4ecce4bbdd17aa7cec97b8131162
Henry-Cha <sungwon4728@naver.com>
2023-02-22 16:13:13 +0900

    refactor: 토큰filter에서 예외처리 메시지 수정

--- src/main/java/org/whitebox/howlook/global/config/security/exception/AccessTokenException.java
+++ src/main/java/org/whitebox/howlook/global/config/security/exception/AccessTokenException.java
@@ -1,8 +1,12 @@
 package org.whitebox.howlook.global.config.security.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.whitebox.howlook.global.error.ErrorCode;
+import org.whitebox.howlook.global.error.ErrorResponse;
 import org.whitebox.howlook.global.error.exception.BusinessException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -21,16 +25,15 @@ public class AccessTokenException extends BusinessException {
     }
 
     public void sendResponseError(HttpServletResponse response){
-
         response.setStatus(this.getErrorCode().getStatus());
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
 
+        final ErrorCode errorCode = this.getErrorCode();
+        final ErrorResponse errorResponse = ErrorResponse.of(errorCode, this.getErrors());
         Gson gson = new Gson();
 
-        String responseStr = gson.toJson(Map.of("msg", this.getErrorCode().getMessage(), "time", new Date()));
-
         try {
-            response.getWriter().println(responseStr);
+            response.getWriter().println(gson.toJson(errorResponse));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
