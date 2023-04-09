2d0f06d850b40eed20bb8ca5cd1b542d03df2c4c
Henry-Cha <sungwon4728@naver.com>
2023-04-09 18:02:51 +0900

    test: MemberRepositoryTests 추가

--- src/test/java/org/whitebox/howlook/domain/member/repository/MemberRepositoryTests.java
+++ src/test/java/org/whitebox/howlook/domain/member/repository/MemberRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.whitebox.howlook.domain.member.dto.UserProfileResponse;
 import org.whitebox.howlook.domain.member.entity.Member;
 import org.whitebox.howlook.domain.member.entity.MemberRole;
 import org.whitebox.howlook.global.config.QuerydslConfig;
@@ -27,7 +28,7 @@ class MemberRepositoryTests {
     @Test
     void joinMemberTest() {
         // given
-        Member member = member();
+        Member member = member("testcode999");
 
         // when
         Member save = memberRepository.save(member);
@@ -39,9 +40,9 @@ class MemberRepositoryTests {
 
     @DisplayName("회원 조회")
     @Test
-    void MemberListTest() {
+    void findByMemberIdTest() {
         // given
-        Member member = memberRepository.save(member());
+        Member member = memberRepository.save(member("testcode999"));
         // when
         Member find = memberRepository.findByMemberId(member.getMemberId()).orElseThrow();
 
@@ -50,11 +51,45 @@ class MemberRepositoryTests {
         assertThat(find.getRoleSet().contains(MemberRole.USER)).isTrue();
     }
 
-    private Member member(){
+    @DisplayName("닉네임 중복 조회")
+    @Test
+    void existNickNameTest() {
+        // given
+        Member member = memberRepository.save(member("testcode999"));
+
+        // when
+        Boolean exist = memberRepository.existsByNickName(member.getNickName());
+
+        // then
+        assertThat(exist).isTrue();
+    }
+
+    @DisplayName("유저프로필 조회 테스트")
+    @Test
+    void findUserProfileTest() {
+        // given
+        Member member = memberRepository.save(member("testcode999"));
+        UserProfileResponse userProfileResponse = userProfileResponse();
+
+        // when
+        UserProfileResponse myProfile = memberRepository.findUserProfileByMemberIdAndTargetMemberId("testcode999","testcode999").orElseThrow();
+        UserProfileResponse profile = memberRepository.findUserProfileByMemberIdAndTargetMemberId("testcode111","testcode999").orElseThrow();
+
+        // then
+        assertThat(myProfile).usingRecursiveComparison().isEqualTo(userProfileResponse); //본인 프로필 확인
+        assertThat(profile).usingRecursiveComparison().isNotEqualTo(userProfileResponse); //타인 프로필 확인
+    }
+
+    private Member member(String name){
         return Member.builder()
-                .memberId("testcode999").memberPassword("a1234567").gender('M').nickName("테스트999")
+                .memberId(name).memberPassword("a1234567").gender('M').nickName(name)
                 .birthDay(LocalDate.now()).del(false).height(180L).weight(70L).name("테스트")
                 .phone("01012345678").profilePhoto(null).social(false).roleSet(Collections.singleton(MemberRole.USER))
                 .build();
+    }
+    private UserProfileResponse userProfileResponse(){
+        return UserProfileResponse.builder()
+                .memberId("testcode999").memberNickName("testcode999").memberHeight(180L).memberWeight(70L)
+                .profilePhoto(null).memberPosts(null).memberPostCount(null).isMe(true).build();
     }
 }
