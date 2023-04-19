69da2e2e429a06deaa622a81aa602f7062570a94
Henry-Cha <sungwon4728@naver.com>
2023-04-20 00:20:24 +0900

    feat: socketConfig rabbitmq 설정추가

--- src/main/java/org/whitebox/howlook/global/config/WebSocketConfig.java
+++ src/main/java/org/whitebox/howlook/global/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package org.whitebox.howlook.global.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -16,10 +17,26 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 //@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${RABBITMQ.USER}")
+    private String rabbitUser;
+    @Value("${RABBITMQ.PWD}")
+    private String rabbitPwd;
+    @Value("${RABBITMQ.HOST}")
+    private String rabbitHost;
+    @Value("${RABBITMQ.PORT}")
+    private int rabbitPort;
+    @Value("${RABBITMQ.VHOST}")
+    private String rabbitVHost;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         //메시지 구독 url
-        config.enableStompBrokerRelay("/exchange");
+        config.enableStompBrokerRelay("/exchange")
+                .setClientLogin(rabbitUser)
+                .setClientPasscode(rabbitPwd)
+                .setRelayHost(rabbitHost)
+                .setRelayPort(rabbitPort)
+                .setVirtualHost(rabbitVHost);
         //메시지 발행 url
         config.setPathMatcher(new AntPathMatcher("."));
         config.setApplicationDestinationPrefixes("/pub");
