3e30802af8e7618098746ffad88c83e19d6732ea
Henry-Cha <sungwon4728@naver.com>
2023-05-25 15:50:35 +0900

    test: 회원가입 controller 테스트 필터제외

--- src/test/java/org/whitebox/howlook/domain/member/controller/MemberAccountControllerTests1.java
+++ src/test/java/org/whitebox/howlook/domain/member/controller/MemberAccountControllerTests1.java
@@ -6,11 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,12 +22,12 @@ import java.time.LocalDate;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 //webMvc 슬라이스 테스트
 @WebMvcTest(MemberAccountController.class)
 @MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureMockMvc(addFilters = false)
 @Slf4j
 class MemberAccountControllerTests1 {
     @Autowired
@@ -41,14 +41,14 @@ class MemberAccountControllerTests1 {
 
     @DisplayName("회원가입 성공")
     @Test
-    @WithMockUser(roles = "USER")
+    //@WithMockUser(roles = "USER")
     void joinPOST() throws Exception {
         //given
         MemberJoinDTO memberJoinDTO = joinDTO();
 
         //when
         ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post("/account/join").with(csrf())
+                MockMvcRequestBuilders.post("/account/join")//.with(csrf())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(memberJoinDTO)));
 
